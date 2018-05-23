96be49ea95e48cfb7a58f74474638f54ddf18f30
Yaroslav <boychenk0@ukr.net>
2018-05-23 19:18:32 +0300

    create foundation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  before_action :get_courses
+  
+  def get_courses
+    @courses = Course.all
+  end
+  
 end
---
+++ db/migrate/20180523155841_create_registrations.rb
@@ -0,0 +1,13 @@
+class CreateRegistrations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :registrations do |t|
+      t.references :course, index: true
+      t.string :full_name
+      t.string :company
+      t.string :email
+      t.string :telephone
+  
+      t.timestamps
+    end
+  end
+end
